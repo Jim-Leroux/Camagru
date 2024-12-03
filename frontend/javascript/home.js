9bf55313d1362870517de322926ef8ebc4f823d3
Jim-Leroux <erenne@hotmail.fr>
2024-12-03 16:48:00 +0100

    home page comment gestion done.

--- frontend/javascript/home.js
+++ frontend/javascript/home.js
@@ -1,4 +1,4 @@
-import { reloadCSS, checkSession, getAllPosts, sendLike } from './utils.js'
+import { reloadCSS, checkSession, getAllPosts, sendLike, sendComment, getAllUsers} from './utils.js'
 
 export async function home(container, callback, scrollValue) {
 	const userSession = await checkSession();
@@ -7,6 +7,11 @@ export async function home(container, callback, scrollValue) {
 		window.location.href = '/#login';
 
 	const usersPosts = await getAllPosts();
+
+	const usersList = await getAllUsers()
+
+	console.log(usersList)
+
 
 	container.innerHTML =
 		`<div id="home">
@@ -64,11 +69,11 @@ export async function home(container, callback, scrollValue) {
 					</div>
 					<div id="post-like-comment">
 						<i id="post-like-icon" class="heart-icon fa-solid fa-heart"></i>
-						<i id="comment-icon" class="fa-regular fa-comment" comment-post-id="${post.id}"></i>
+						<i id="post-comment-icon" class="comment fa-regular fa-comment" comment-post-id="${post.id}"></i>
 					</div>
 				</div>
 				<div id="post-bottom">
-					<p id="description"><strong>Name</strong>: ${post.description}</p>
+					<p id="description"><strong>${usersList[post.user_id - 1].username}</strong>: ${post.description}</p>
 				</div>
 			</div>`;
 
@@ -81,6 +86,7 @@ export async function home(container, callback, scrollValue) {
 		}
 
 		const sendLikeIcon = postElement.querySelector("#post-like-icon");
+		const sendCommentIcon = postElement.querySelector("#post-comment-icon");
 
 		sendLikeIcon.addEventListener('click', () => {
 			const postsPos = document.getElementById('posts-container');
@@ -92,6 +98,97 @@ export async function home(container, callback, scrollValue) {
 				postsPos.scrollTop = scrollValue;
 			}, scrollValue);
 		});
+
+		sendCommentIcon.addEventListener('click', () => {
+			const focusedElement = document.createElement('div')
+					focusedElement.setAttribute("id", "focused-section");
+					focusedElement.innerHTML = `
+					<i id="exit-focus-icon" class="fa-solid fa-xmark"></i>
+					<div id="focused-post">
+						<div id="focused-post-img">
+							<img src="${post.post_path}" alt="${post.description}" />
+						</div>
+						<div id="focused-post-section">
+							<div id="comment-display-section">
+
+							</div>
+							<div id="react-section">
+								<p><strong>${post.likes.length}</strong> likes</p>
+								<div id="like-comment-section">
+									<i id="focused-like-icon" class="fa-solid fa-heart"></i>
+									<form id="post-comment-form" action="post-comment" method="POST">
+										<label for="comment-input-form"></label>
+										<input type="text" id="comment-input-form" name="comment-input-form" placeholder="Add a comment..." required>
+										<p id="send-comment"><strong>Post</strong></p>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+					`;
+					postsContainer.appendChild(focusedElement);
+
+					const sendLikeIcon = document.getElementById("focused-like-icon");
+					const exitButton = document.getElementById("exit-focus-icon");
+					const focusedSection = document.getElementById("focused-section");
+					const commentDisplay = document.getElementById("comment-display-section");
+					const sendCommentButton = document.getElementById("send-comment");
+
+					for (let like = 0; post.likes[like]; like++) {
+						if (post.likes[like].user_id == document.cookie.split('=')[1]) {
+							sendLikeIcon.style.color = "red";
+							break;
+						}
+					}
+
+					for (let c = 0; post.comments[c]; c++) {
+						const userComment = document.createElement('p');
+						userComment.setAttribute("id", "userComment")
+						userComment.innerHTML = `
+						<strong>${usersList[post.comments[c].user_id -1].username}:</strong> ${post.comments[c].comment}
+						`;
+						commentDisplay.appendChild(userComment);
+					}
+
+					exitButton.addEventListener('click', () => {
+						focusedSection.remove();
+					})
+
+					sendLikeIcon.addEventListener('click', () => {
+						const postsPos = document.getElementById('posts-container');
+						scrollValue = postsPos.scrollTop;
+						sendLike(post.id);
+						home(container, () => {
+							const postsPos = document.getElementById('posts-container');
+							postsPos.scrollTop = scrollValue;
+							const userPosts = document.querySelectorAll('.comment');
+							userPosts[post.id - 1].click();
+						}, scrollValue);
+					})
+
+					const form = document.getElementById('post-comment-form');
+					const commentInput = document.getElementById('comment-input-form');
+
+					sendCommentButton.addEventListener('click', () => {
+						if (form.checkValidity()) {
+							const postsPos = document.getElementById('posts-container');
+							scrollValue = postsPos.scrollTop;
+							const commentValue = commentInput.value;
+							const post_id = post.id;
+							sendComment(post_id, commentValue);
+
+							home(container, () => {
+								const postsPos = document.getElementById('posts-container');
+								postsPos.scrollTop = scrollValue;
+								const userPosts = document.querySelectorAll('.comment');
+								userPosts[post.id - 1].click();
+							}, scrollValue);
+						} else {
+							form.reportValidity();
+						}
+					});
+
+		})
 
 		postsContainer.appendChild(postElement);
 	}
